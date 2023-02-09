3057ec41dc0684af41a6752eb33298f5da615eda
subin <qotnqls77@naver.com>
2023-02-09 17:20:53 +0900

    hw 0209

--- algo/src/algo/baek/b12891_dna.java
+++ algo/src/algo/baek/b12891_dna.java
@@ -1,7 +1,26 @@
 package algo.baek;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class b12891_dna {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = null;
+		StringBuilder sb = new StringBuilder();
+		    
+		st = new StringTokenizer(br.readLine());
+		
+		int s = Integer.parseInt(st.nextToken());
+		int p = Integer.parseInt(st.nextToken());
+		
+		//비밀번호는 아마 중복 순열일듯
+		
+		for(int i=0;i<s;i++) {
+			
+		}
 		
 	}
 }
---
+++ algo/src/algo/baek/b2961_food.java
@@ -0,0 +1,71 @@
+package algo.baek;
+
+import java.util.Scanner;
+
+public class b2961_food {
+	static int[] sin;
+	static int[] ssun;
+	static int[] sin_sum;
+	static int[] ssun_sum;
+    static boolean[] isSelected;  //각 원소가 부분집합의 구성에 포함되는지 여부
+    static int n;
+    static int min=100000000;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		//신맛은 곱. 쓴맛은 합
+		n=sc.nextInt(); //재료의 개수
+		
+		//초기화
+		sin = new int[n];
+		ssun = new int[n];
+		
+		sin_sum = new int[n];
+		ssun_sum = new int[n];
+		
+		isSelected = new boolean[n];
+		
+		for(int i=0;i<n;i++) {
+			sin[i] = sc.nextInt();	
+			ssun[i] = sc.nextInt();
+		}
+		generateSubSet(0);
+		System.out.println(min);
+	}
+	
+	private static void generateSubSet(int cnt) { //cnt : 직전까지 고려된 원소 수
+        if(cnt == n) {
+        	int tmp1 = 1;
+			int tmp2 = 0;
+			
+            for (int i = 0; i < n; i++) {	
+            	
+            	if(isSelected[i]) {
+            		sin_sum[i]=tmp1*sin[i]; //tmp는 직전의 값 !! 바뀌기 전의 값 ... 
+        			ssun_sum[i]=tmp2+ssun[i];
+        				
+        			tmp1 = sin_sum[i];
+        			tmp2 = ssun_sum[i];
+        				
+            		//for문으로 현재 true 인값 다 곱하거나 더하기 
+            		int tmp = Math.abs(sin_sum[i]-ssun_sum[i]);
+      
+            		if(tmp  < min)
+            			min = tmp;	
+            	}
+            }
+          
+            return;
+        }
+        
+        // 현재 원소를 부분집합에 포함
+        isSelected[cnt] = true;
+        generateSubSet(cnt+1);
+        
+        // 미포함
+        isSelected[cnt] = false;
+        generateSubSet(cnt+1);
+        
+        
+    }
+}
