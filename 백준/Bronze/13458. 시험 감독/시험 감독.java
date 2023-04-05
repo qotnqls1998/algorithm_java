688eb4ea6ef9de1364dfb9ac41e61629a7ec8b81
qotnqls1998 <124232457+qotnqls1998@users.noreply.github.com>
2023-04-05 17:31:58 +0900

    [Bronze II] Title: 시험 감독, Time: 1556 ms, Memory: 307584 KB -BaekjoonHub

---
+++ 백준/Bronze/13458. 시험 감독/시험 감독.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] student = new int[n];
+		long cnt =0;
+		
+		for(int i=0;i<n;i++) {
+			student[i]=sc.nextInt();
+		}
+		int b = sc.nextInt();
+		double c = sc.nextInt();	
+		
+		for(int i=0;i<n;i++) {
+			if(student[i]>0) {
+				student[i]-=b;
+				cnt++;
+				}
+
+			if (student[i]>0) {
+				cnt+= student[i]/c ;
+				if(student[i]%c!=0) cnt++;
+			}
+		}
+		System.out.println(cnt);
+	}
+}
