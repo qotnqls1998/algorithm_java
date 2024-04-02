a67f9e73a6253fb2a7fd5e35e48df211f61e7c9b
SuBeen Bae <124232457+qotnqls1998@users.noreply.github.com>
2024-04-02 18:20:18 +0900

    [level 2] Title: 타겟 넘버, Time: 0.46 ms, Memory: 79.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -0,0 +1,33 @@
+import java.lang.*;
+import java.io.*;
+import java.util.*;
+    
+class Solution {
+    static int number;
+    static int[] arr;
+    static int dep;
+    static int sum;
+    static int answer;
+    
+    public int solution(int[] numbers, int target) {
+        //선언
+        arr = new int[numbers.length];
+        number = target; 
+        arr = numbers;
+        
+        dfs(0, 0);
+        
+        return answer;
+    }
+    private void dfs(int dep, int sum){
+        
+        //5개 다봤으면 꺼져
+        if(dep==arr.length){
+           if(number == sum) answer++;
+        }
+        else{
+            dfs(dep+1,sum+arr[dep]);
+            dfs(dep+1, sum-arr[dep]);
+        }
+    }
+}
